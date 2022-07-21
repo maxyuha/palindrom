5a2f257fde3e661d5fe72b94550d0822d7ad6691
Maksim <maxyuha1998@gmail.com>
2022-07-21 14:14:18 +0300

    made task 19

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsolleApp
+{
+     class Program
+     {
+        static void Main(string[] args)
+        {
+            void Zadacha19()
+            {
+               //Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом. 
+               int number=Convert.ToInt32(Console.ReadLine());
+               int number2= number%10;
+               int number3=number/10%10;
+               int number4=number/1000%10;
+               int number5=number/10000;
+               if (number2==number5&&number3==number4)
+               {
+                Console.WriteLine("палиндром");
+               }
+               else Console.WriteLine("не является палиндромом");
+          
+            }
+            Zadacha19();
+        }
+
+        
+     }
+    
+}
